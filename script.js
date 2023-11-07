0bcf9e1ad07dc32f5fc16ddd723992e57bd52b6e
Caiorossi00 <caio_rossi56@hotmail.com>
2023-11-07 05:48:52 -0300

    Contador Atualizado

--- script.js
+++ script.js
@@ -13,8 +13,7 @@
 //   backgroundInfo2.classList.add("light-mode-table2");
 //   backgroundInfo3.classList.add("light-mode-inserir-dados");
 // }
-
-let contador = 0;
+let contador = JSON.parse(localStorage.getItem("contador")) || 0;
 
 const transacoes = JSON.parse(localStorage.getItem("transacoes")) || [];
 const dataMeses = JSON.parse(localStorage.getItem("transacoesData")) || {
@@ -234,6 +233,7 @@ function passarMes() {
   const alterarMes = document.querySelector(".display-mes");
   if (contador < 11) {
     contador++;
+    localStorage.setItem("contador", JSON.stringify(contador));
     alterarMes.innerText = chaves[contador];
     exibirTransacoes();
   }
@@ -246,6 +246,7 @@ function voltarMes() {
   const alterarMes = document.querySelector(".display-mes");
   if (contador > 0) {
     contador--;
+    localStorage.setItem("contador", JSON.stringify(contador));
     alterarMes.innerText = chaves[contador];
     exibirTransacoes();
   }
